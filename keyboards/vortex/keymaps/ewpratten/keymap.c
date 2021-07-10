201834a425f92ec0a7cab79906d185e2d82bcf58
Evan Pratten <ewpratten@gmail.com>
2021-07-10 13:36:32 -0400

    Add some mappings to make r6 easier

--- keyboards/vortex/keymaps/ewpratten/keymap.c
+++ keyboards/vortex/keymaps/ewpratten/keymap.c
@@ -1,6 +1,6 @@
 /*
  * Evan Pratten's keymaps for the Vortex Core keyboard.
- * 
+ *
  * All contents of this file are custom, and subject to wild change. Good luck.
  */
 
@@ -12,7 +12,6 @@ enum custom_keycodes {
     CK_BUILD,
     CK_VSCODE_MENU,
     CK_MACRO_MUTE,
-    CK_PASTE_SHRUGGIE,
     CK_PASTE_RICKROLL
 };
 
@@ -33,30 +32,40 @@ const uint16_t keymaps_default[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______
     ),
 #elif defined(KEYMAP_VORTEX_CORE)
+
+    // No modifiers
     [0] = LAYOUT_core(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  KC_BSPC,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSPC, MO(2),
         KC_LCTL, KC_LALT, KC_LGUI, MO(3),   KC_SPC,  KC_SPC,  MO(1),   KC_RALT, MO(4),  KC_RCTL
     ),
+
+    // Super button on the right of the space bars
     [1] = LAYOUT_core(
         _______, KC__VOLDOWN, KC__VOLUP, KC__MUTE, _______, _______, _______, KC_PGUP, _______,   KC_PGDN, KC_PSCR, KC_SLCK, KC_PAUS,
         KC_CAPS, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,KC_INS,  _______,
         _______, _______, _______, _______, _______, KC_HOME, KC_END,  _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
+
+    // FN1 button
     [2] = LAYOUT_core(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
         _______, _______, _______, _______, _______, KC_QUOTE, KC_SLSH, KC_LBRC, KC_RBRC, KC_BSLS, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
+    // FN button
+    //
+    // Note: KC_5 and KC_6 hang out on my macro layer to make be a better R6 player. Don't ask questions
     [3] = LAYOUT_core(
         _______,  _______,_______, _______, CK_PASTE_RICKROLL, CK_COPY_TO_NEW_TAB, _______, _______, _______, _______, CK_VSCODE_MENU, _______, _______,
-        _______, _______, CK_PASTE_SHRUGGIE, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, KC_5, KC_6, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, CK_BUILD, _______, CK_MACRO_MUTE, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
+    // PN button
     [4] = LAYOUT_core(
         _______,  _______,_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -107,11 +116,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case CK_MACRO_MUTE:
             if (record->event.pressed) {
                 SEND_STRING(SS_DOWN(X_LCTRL)SS_DOWN(X_LSHIFT)SS_DOWN(X_F24)SS_UP(X_F24)SS_UP(X_LCTRL)SS_UP(X_LSHIFT));
-            }
-            break;
-        case CK_PASTE_SHRUGGIE:
-            if (record->event.pressed) {
-                SEND_STRING("¯\\_(ツ)_/¯");
             }
             break;
         case CK_PASTE_RICKROLL:
